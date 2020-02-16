a4164c49820af08866bf19f0f24dd90217eb30a9
Laxman Ram <laxman.ram@iconnectsolutions.com>
2020-02-16 14:35:21 +0530

    chart screen modified

--- screens/ChatDetailsScreen.js
+++ screens/ChatDetailsScreen.js
@@ -1,153 +1,264 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  Alert,
-  ScrollView,
-  TextInput,
-  FlatList,
-  Button
-} from 'react-native';
+import { Text, View, StyleSheet, ScrollView, KeyboardAvoidingView, TextInput, TouchableHighlight, Keyboard } from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import AutogrowInput from 'react-native-autogrow-input';
 
-export default class ChatDetails extends Component {
+//used to make random-sized messages
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// The actual chat view itself- a ScrollView of BubbleMessages, with an InputBar at the bottom, which moves with the keyboard
+export default class ChatView extends Component {
 
   constructor(props) {
     super(props);
+
+    var loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac orci augue. Sed fringilla nec magna id hendrerit. Proin posuere, tortor ut dignissim consequat, ante nibh ultrices tellus, in facilisis nunc nibh rutrum nibh.';
+
+    //create a set number of texts with random lengths. Also randomly put them on the right (user) or left (other person).
+    var numberOfMessages = 20;
+
+    var messages = [];
+
+    for(var i = 0; i < numberOfMessages; i++) {
+      var messageLength = getRandomInt(10, 120);
+
+      var direction = getRandomInt(1, 2) === 1 ? 'right' : 'left';
+
+      message = loremIpsum.substring(0, messageLength);
+
+      messages.push({
+        direction: direction,
+        text: message
+      })
+    }
+
     this.state = {
-      data: [
-        {id:1, date:"9:50 am", type:'in',  message: "Lorem ipsum dolor sit amet"},
-        {id:2, date:"9:50 am", type:'out', message: "Lorem ipsum dolor sit amet"} ,
-        {id:3, date:"9:50 am", type:'in',  message: "Lorem ipsum dolor sit a met"}, 
-        {id:4, date:"9:50 am", type:'in',  message: "Lorem ipsum dolor sit a met"}, 
-        {id:5, date:"9:50 am", type:'out', message: "Lorem ipsum dolor sit a met"}, 
-        {id:6, date:"9:50 am", type:'out', message: "Lorem ipsum dolor sit a met"}, 
-        {id:7, date:"9:50 am", type:'in',  message: "Lorem ipsum dolor sit a met"}, 
-        {id:8, date:"9:50 am", type:'in',  message: "Lsgsgsdggsdg sdgsdgdsg weegewgwg orem ipsum dolor sit a met"},
-        {id:9, date:"9:50 am", type:'in',  message: "Lorem ipsum dolor sit a met"},
-      ]
-    };
+      messages: messages,
+      inputBarText: ''
+    }
   }
 
-  renderDate = (date) => {
-    return(
-      <Text style={styles.time}>
-        {date}
-      </Text>
-    );
+  static navigationOptions = {
+    title: 'Chat',
+  };
+
+  //fun keyboard stuff- we use these to get the end of the ScrollView to "follow" the top of the InputBar as the keyboard rises and falls
+  componentWillMount () {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow.bind(this));
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+
+  //When the keyboard appears, this gets the ScrollView to move the end back "up" so the last message is visible with the keyboard up
+  //Without this, whatever message is the keyboard's height from the bottom will look like the last message.
+  keyboardDidShow (e) {
+    this.scrollView.scrollToEnd();
+  }
+
+  //When the keyboard dissapears, this gets the ScrollView to move the last message back down.
+  keyboardDidHide (e) {
+    this.scrollView.scrollToEnd();
+  }
+
+  //scroll to bottom when first showing the view
+  componentDidMount() {
+    setTimeout(function() {
+      this.scrollView.scrollToEnd();
+    }.bind(this))
+  }
+
+  //this is a bit sloppy: this is to make sure it scrolls to the bottom when a message is added, but 
+  //the component could update for other reasons, for which we wouldn't want it to scroll to the bottom.
+  componentDidUpdate() {
+    setTimeout(function() {
+      this.scrollView.scrollToEnd();
+    }.bind(this))
+  }
+
+  _sendMessage() {
+    this.state.messages.push({direction: "right", text: this.state.inputBarText});
+
+    this.setState({
+      messages: this.state.messages,
+      inputBarText: ''
+    });
+  }
+
+  _onChangeInputBarText(text) {
+    this.setState({
+      inputBarText: text
+    });
+  }
+
+  //This event fires way too often.
+  //We need to move the last message up if the input bar expands due to the user's new message exceeding the height of the box.
+  //We really only need to do anything when the height of the InputBar changes, but AutogrowInput can't tell us that.
+  //The real solution here is probably a fork of AutogrowInput that can provide this information.
+  _onInputSizeChange() {
+    setTimeout(function() {
+      this.scrollView.scrollToEnd({animated: false});
+    }.bind(this))
   }
 
   render() {
 
-    return (
-      <View style={styles.container}>
-        <FlatList style={styles.list}
-          data={this.state.data}
-          keyExtractor= {(item) => {
-            return item.id;
-          }}
-          renderItem={(message) => {
-            console.log(item);
-            const item = message.item;
-            let inMessage = item.type === 'in';
-            let itemStyle = inMessage ? styles.itemIn : styles.itemOut;
-            return (
-              <View style={[styles.item, itemStyle]}>
-                {!inMessage && this.renderDate(item.date)}
-                <View style={[styles.balloon]}>
-                  <Text>{item.message}</Text>
-                </View>
-                {inMessage && this.renderDate(item.date)}
-              </View>
-            )
-          }}/>
-        <View style={styles.footer}>
-          <View style={styles.inputContainer}>
-            <TextInput style={styles.inputs}
-                placeholder="Write a message..."
-                underlineColorAndroid='transparent'
-                onChangeText={(name_address) => this.setState({name_address})}/>
-          </View>
+    var messages = [];
 
-            <TouchableOpacity style={styles.btnSend}>
-              <Image source={{uri:"https://png.icons8.com/small/75/ffffff/filled-sent.png"}} style={styles.iconSend}  />
-            </TouchableOpacity>
-        </View>
-      </View>
-    );
+    this.state.messages.forEach(function(message, index) {
+      messages.push(
+          <MessageBubble key={index} direction={message.direction} text={message.text}/>
+        );
+    });
+
+    return (
+              <View style={styles.outer}>
+                  <ScrollView ref={(ref) => { this.scrollView = ref }} style={styles.messages}>
+                    {messages}
+                  </ScrollView>
+                  <InputBar onSendPressed={() => this._sendMessage()} 
+                            onSizeChange={() => this._onInputSizeChange()}
+                            onChangeText={(text) => this._onChangeInputBarText(text)}
+                            text={this.state.inputBarText}/>
+                  <KeyboardSpacer/>             
+              </View>
+            );
   }
 }
 
+//The bubbles that appear on the left or the right for the messages.
+class MessageBubble extends Component {
+  render() {
+
+    //These spacers make the message bubble stay to the left or the right, depending on who is speaking, even if the message is multiple lines.
+    var leftSpacer = this.props.direction === 'left' ? null : <View style={{width: 70}}/>;
+    var rightSpacer = this.props.direction === 'left' ? <View style={{width: 70}}/> : null;
+
+    var bubbleStyles = this.props.direction === 'left' ? [styles.messageBubble, styles.messageBubbleLeft] : [styles.messageBubble, styles.messageBubbleRight];
+
+    var bubbleTextStyle = this.props.direction === 'left' ? styles.messageBubbleTextLeft : styles.messageBubbleTextRight;
+
+    return (
+        <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+            {leftSpacer}
+            <View style={bubbleStyles}>
+              <Text style={bubbleTextStyle}>
+                {this.props.text}
+              </Text>
+            </View>
+            {rightSpacer}
+          </View>
+      );
+  }
+}
+
+//The bar at the bottom with a textbox and a send button.
+class InputBar extends Component {
+
+  //AutogrowInput doesn't change its size when the text is changed from the outside.
+  //Thus, when text is reset to zero, we'll call it's reset function which will take it back to the original size.
+  //Another possible solution here would be if InputBar kept the text as state and only reported it when the Send button
+  //was pressed. Then, resetInputText() could be called when the Send button is pressed. However, this limits the ability
+  //of the InputBar's text to be set from the outside.
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.text === '') {
+      this.autogrowInput.resetInputText();
+    }
+  }
+
+  render() {
+    return (
+          <View style={styles.inputBar}>
+            <AutogrowInput style={styles.textBox}
+                        ref={(ref) => { this.autogrowInput = ref }} 
+                        multiline={true}
+                        defaultHeight={30}
+                        onChangeText={(text) => this.props.onChangeText(text)}
+                        onContentSizeChange={this.props.onSizeChange}
+                        value={this.props.text}/>
+            <TouchableHighlight style={styles.sendButton} onPress={() => this.props.onSendPressed()}>
+                <Text style={{color: 'white'}}>Send</Text>
+            </TouchableHighlight>
+          </View> 
+          );
+  }
+}
+
+//TODO: separate these out. This is what happens when you're in a hurry!
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+
+  //ChatView
+
+  outer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: 'white'
   },
-  list:{
-    paddingHorizontal: 17,
+
+  messages: {
+    flex: 1
   },
-  footer:{
+
+  //InputBar
+
+  inputBar: {
     flexDirection: 'row',
-    height:60,
-    backgroundColor: '#eeeeee',
-    paddingHorizontal:10,
-    padding:5,
+    justifyContent: 'space-between',
+    paddingHorizontal: 5,
+    paddingVertical: 3,
   },
-  btnSend:{
-    backgroundColor:"#00BFFF",
-    width:40,
-    height:40,
-    borderRadius:360,
-    alignItems:'center',
-    justifyContent:'center',
+
+  textBox: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    flex: 1,
+    fontSize: 16,
+    paddingHorizontal: 10
   },
-  iconSend:{
-    width:30,
-    height:30,
-    alignSelf:'center',
-  },
-  inputContainer: {
-    borderBottomColor: '#F5FCFF',
-    backgroundColor: '#FFFFFF',
-    borderRadius:30,
-    borderBottomWidth: 1,
-    height:40,
-    flexDirection: 'row',
-    alignItems:'center',
-    flex:1,
-    marginRight:10,
-  },
-  inputs:{
-    height:40,
-    marginLeft:16,
-    borderBottomColor: '#FFFFFF',
-    flex:1,
-  },
-  balloon: {
-    maxWidth: 250,
-    padding: 15,
-    borderRadius: 20,
-  },
-  itemIn: {
-    alignSelf: 'flex-start'
-  },
-  itemOut: {
-    alignSelf: 'flex-end',
+
+  sendButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 15,
+    marginLeft: 5,
+    paddingRight: 15,
+    borderRadius: 5,
     backgroundColor: '#3787db'
   },
-  time: {
-    alignSelf: 'flex-end',
-    margin: 15,
-    fontSize:12,
-    color:"#808080",
+
+  //MessageBubble
+
+  messageBubble: {
+      borderRadius: 5,
+      marginTop: 8,
+      marginRight: 10,
+      marginLeft: 10,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      flexDirection:'row',
+      flex: 1
   },
-  item: {
-    marginVertical: 14,
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor:"#eeeeee",
-    borderRadius:300,
-    padding:5,
+
+  messageBubbleLeft: {
+    backgroundColor: '#d5d8d4',
   },
-});
+
+  messageBubbleTextLeft: {
+    color: 'black'
+  },
+
+  messageBubbleRight: {
+    backgroundColor: '#3787db'
+  },
+
+  messageBubbleTextRight: {
+    color: 'white'
+  },
+})
