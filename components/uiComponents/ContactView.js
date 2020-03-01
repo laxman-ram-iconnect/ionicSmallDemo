92b5be1212d3231a4a0db24f5b58a2f1f3693dea
Laxman Ram <laxman.ram@iconnectsolutions.com>
2020-03-01 09:25:13 +0530

    contact view added

---
+++ components/uiComponents/ContactView.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+import ImageAvatar from './ImageAvatar';
+import TitleAvatar from './TitleAvatar';
+
+
+export default ContactView = (props) => {
+    return (
+        <TouchableNativeFeedback onPress={props.onClickFunction}>
+
+            <View style={styles.imageRow}>
+                <View style={{ flexDirection: 'row' }}>
+                    {
+                        props.url ?
+                            <ImageAvatar
+                                height={50}
+                                width={50}
+                                borderRadius={50}
+                                url={props.url}
+                            /> : <TitleAvatar height={50}
+                                width={50}
+                                borderRadius={50}
+                                backgroundColor={'lightgrey'}
+                                title={props.avatarTitle} />
+                    }
+
+
+                    <View style={styles.annaFailStackColumn}>
+                        <View style={styles.annaFailStack}>
+                            <Text style={styles.annaFail}>{props.mainLabel}</Text>
+                        </View>
+                        <Text style={styles.loremIpsum}>{props.subLabel}</Text>
+                    </View>
+                </View>
+            </View>
+        </TouchableNativeFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(255,255,255,1)"
+
+    },
+    text: {
+        color: "#121212",
+        fontSize: 24,
+        // fontFamily: "roboto-regular",
+        textAlign: "center",
+        marginTop: 310,
+        alignSelf: "center"
+    },
+    image1: {
+        // width: 50,
+        // height: 50,
+        // borderRadius: 50,
+        // borderColor: "#000000",
+        // borderWidth: 1
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        borderColor: "#000000",
+        borderWidth: 1,
+        // overflow: 'hidden'
+    },
+    annaFail: {
+        top: 0,
+        left: 0,
+        width: 180,
+        height: 22,
+        color: "#121212",
+        position: "absolute",
+        fontWeight: '700',
+        // justifyContent: "space-between",
+        fontSize: 17,
+        // fontFamily: "roboto-700",
+        // lineHeight: 14
+    },
+    icon: {
+        marginTop: 3,
+        color: "#3888db",
+        fontSize: 18,
+        width: 18,
+        height: 18
+    },
+    annaFailStack: {
+        width: 188,
+        height: 30
+    },
+    loremIpsum: {
+        width: 170,
+        height: 20,
+        color: "rgba(155,155,155,1)",
+        fontSize: 15,
+        // fontFamily: "roboto-regular",
+        lineHeight: 14
+    },
+    annaFailStackColumn: {
+        minWidth: 188,
+        marginLeft: 18,
+        marginTop: 3,
+        marginBottom: 1
+    },
+    loremIpsum3: {
+        width: 66,
+        height: 22,
+        color: "rgba(155,155,155,1)",
+        fontSize: 15,
+        marginTop: -5
+        // fontFamily: "roboto-regular",
+        // lineHeight: 14
+    },
+    text2: {
+        width: 13,
+        height: 13,
+        color: "rgba(155,155,155,1)",
+        fontSize: 15,
+        // fontFamily: "roboto-regular",
+        lineHeight: 14,
+        marginLeft: 33
+    },
+    loremIpsum3Column: {
+        width: 66,
+        marginLeft: 3,
+        marginTop: 7,
+        marginBottom: 4,
+        flexDirection: 'column',
+        alignItems: 'flex-end'
+    },
+    imageRow: {
+        height: 82,
+        flexDirection: "row",
+        padding: 16,
+        // marginLeft: 14,
+        // marginRight: 14,
+        justifyContent: 'space-between'
+    },
+    rightListContent: {
+        flexDirection: 'row'
+    }
+});
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -3,41 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import SearchBarInput from '../components/SearchBar';
-import ListItemWithAvatar, { ListItemAvatar } from '../components/AvatarForList';
+import ContactView from '../components/uiComponents/ContactView';
 import { contactData } from '../constants/dummyData';
 
 export default function LinksScreen() {
   return (
     <View style={styles.container}>
-      <SearchBarInput/>
+      <SearchBarInput />
 
       <ScrollView style={styles.container}>
-      {
-    contactData.map((l, i) => (
-      <React.Fragment>
-        <Text style={{backgroundColor: '#fff', marginLeft: 16, marginTop: 3, 
-        color: 'rgba(20, 20, 20, 0.7)'}}
-        key={i}>{l.group}</Text>
-        { l.data.map((ins, indx) => (
-          <View key={indx + 1}>
-            <ListItemWithAvatar
-              leftAvatar={ <ListItemAvatar 
-                        showRounded={true}
-                        size={'medium'}
-                        uri={ins.avatar_url}
-                        title={ins.name.substring(0, 2).toUpperCase()} /> } 
-              {...ins} 
-              index={indx + l.group} 
-              key={indx+ 6}
-              containerStyle={{ paddingTop: 14, paddingBottom: 14 }}/>
-            <View key={indx + 2} style={{ borderBottomColor: "#ececec",marginLeft:70,marginRight:10,marginTop:-5,borderBottomWidth: 1}}></View>
-            </View>
-            
-          // <Text>{i}</Text>
-          )) }
-      </React.Fragment>
-    ))
-  }
+        {
+          contactData.map((l, i) => (
+            <React.Fragment>
+              <Text style={{
+                backgroundColor: '#fff', marginLeft: 16, marginTop: 3,
+                color: 'rgba(20, 20, 20, 0.7)'
+              }}
+                key={i}>{l.group}</Text>
+              {l.data.map((ins, indx) => (
+                <View key={indx + 1}>
+                  <ContactView
+                    url={ins.avatar_url}
+                    avatarTitle={ins.name.substring(0, 2).toUpperCase()}
+                    mainLabel={ins.name}
+                  />
+                  <View key={indx + 2} style={{ borderBottomColor: "#ececec", marginLeft: 70, marginRight: 16, marginTop: -5, borderBottomWidth: 1 }}></View>
+                </View>
+
+                // <Text>{i}</Text>
+              ))}
+            </React.Fragment>
+          ))
+        }
       </ScrollView>
 
     </View>
